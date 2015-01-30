78966f521da1ba4fa57a2596c6336693dea5db8b
samjhooker <samjhooker@gmail.com>
2015-01-30 21:59:50 +1300

    Initial Commit

---
+++ SocialMediaApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SocialMediaApp
+//
+//  Created by Samuel Hooker on 30/01/15.
+//  Copyright (c) 2015 Jocus Interactive. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
