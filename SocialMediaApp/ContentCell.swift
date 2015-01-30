5b12b69a8b17703f9d2c224db994ea43836cf4c0
samjhooker <samjhooker@gmail.com>
2015-01-30 22:56:58 +1300

    Created basic Scenes and cell types

---
+++ SocialMediaApp/ContentCell.swift
@@ -0,0 +1,14 @@
+//
+//  ContentCell.swift
+//  SocialMediaApp
+//
+//  Created by Samuel Hooker on 30/01/15.
+//  Copyright (c) 2015 Jocus Interactive. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class ContentCell: UITableViewCell {
+
+}
---
+++ SocialMediaApp/TextCell.swift
@@ -0,0 +1,14 @@
+//
+//  TextCell.swift
+//  SocialMediaApp
+//
+//  Created by Samuel Hooker on 30/01/15.
+//  Copyright (c) 2015 Jocus Interactive. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class TextCell: UITableViewCell {
+
+}
